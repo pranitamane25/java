3fdc122e541111e8e2f465157045554dfb1233e9
pranitamane25 <pranitamane2506@gmail.com>
2026-08-13 09:55:22 +0530

    sum od two numbers through user input

---
+++ Day 1/sum.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class sum{
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number");
+
+        int a=sc.nextInt();
+        System.out.println("Enter second number");
+        int b=sc.nextInt();
+        
+        int sum=a+b;
+
+        System.out.println("Sum of the above numbers:" +sum);
+
+
+    }
+    
+}
