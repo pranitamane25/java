33a341d5f75b37d57161fa8b71fc5c11badc4e48
pranitamane25 <pranitamane2506@gmail.com>
2026-08-19 12:54:49 +0530

    sum of the array elements

---
+++ Day 3/SUM.java
@@ -0,0 +1,15 @@
+public class SUM{
+    public static void main(String[] args){
+
+        int[] arr={10,20,30,40};
+        int sum=0;
+
+        for(int i=0;i<arr.length;i++){
+
+            sum=sum+arr[i];
+        }
+
+        System.out.println("sum of the elements:" +sum );
+
+    }
+}
