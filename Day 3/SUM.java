30a3c7fbcf137a8d8ba479364a9aab1ed5d6618f
pranitamane25 <pranitamane2506@gmail.com>
2026-08-19 13:15:46 +0530

    find largest element from array

--- Day 3/SUM.java
+++ Day 3/SUM.java
@@ -1,11 +1,11 @@
 public class SUM{
     public static void main(String[] args){
 
-        int[] arr={10,20,30,40};
+        int[] arr={10,20,30,40,50,100};
         int sum=0;
 
-        for(int i=0;i<arr.length;i++){
-
+        for(int i=0;i<arr.length;i++)
+        {
             sum=sum+arr[i];
         }
 
