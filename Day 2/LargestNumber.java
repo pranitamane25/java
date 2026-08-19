9b66aa56a81d070db82ba7999402fa6723049bc9
pranitamane25 <pranitamane2506@gmail.com>
2026-08-19 11:49:36 +0530

    leap year or not

--- Day 2/LargestNumber.java
+++ Day 2/LargestNumber.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class LargestNumber {
     public static void main(String[]  args){
         Scanner sc=new Scanner(System.in);
-
-
         System.out.println("Enter value of a:");
         int a=sc.nextInt();
          System.out.println("Enter value of b:");
