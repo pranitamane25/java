bdcb51c308fd397667efcd898885c21c122d2c85
pranitamane25 <pranitamane2506@gmail.com>
2026-08-21 11:48:14 +0530

    how many times number is occured

--- Day 4/CountOccurences.java
+++ Day 4/CountOccurences.java
@@ -2,11 +2,12 @@ public class CountOccurences {
     public static void main(String[] args){
 
         int[] arr={10,20,10,2,10,6,89};
+        int target=10;
 
         int count=0;
 
         for(int i=0;i<arr.length;i++){
-            if(arr[i]==10){
+            if(arr[i]==target){
                 count++;
             }
         }
