9708466c6e07c1e90c3873733a88f3ee20d76912
pranitamane25 <pranitamane2506@gmail.com>
2026-08-21 11:32:09 +0530

    find index of the element

--- Day 4/LinearSearch.java
+++ Day 4/LinearSearch.java
@@ -7,7 +7,7 @@ public class LinearSearch{
 
         for(int i=0;i<arr.length;i++){
             if(arr[i]==target){
-                System.out.println("Element found:" +target );
+                System.out.println("Element found");
             }
 
         }
